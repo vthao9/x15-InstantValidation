b6748e99043b1bd1867b5ca88d0f3b3cc8707417
VEE <v_thao9@mail.csuchico.edu>
2016-10-12 17:26:14 -0700

    fix validation of user and passEntered

--- form-validation.js
+++ form-validation.js
@@ -2,9 +2,16 @@ function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
-if (userEntered.length <= 6) {
+if (userEntered.length < 6) {
   //Show message that there is an error with the username...
   document.getElementById("usernameError").innerHTML="Bad username.";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameError").classList.add("shown-message");
+  //Turn the username items red
+  document.getElementById("usernameGroup").classList.add("has-error");
+}
+else if (userEntered.includes(" ")) {
+  document.getElementById("usernameError").innerHTML="cant have space";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
@@ -13,9 +20,23 @@ if (userEntered.length <= 6) {
   else {
     document.getElementById("usernameGroup").classList.add("has-success");
 }
-if (passEntered == "password") {
+if (passEntered == "password" || passEntered == "PASSWORD" || passEntered == userEntered) {
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordError").classList.add("shown-message");
+  //Turn the password items red
+  document.getElementById("passwordGroup").classList.add("has-error");
+}
+else if (passEntered.length < 6) {
+  document.getElementById("passwordError").innerHTML="password cant be less than 6 characters";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordError").classList.add("shown-message");
+  //Turn the password items red
+  document.getElementById("passwordGroup").classList.add("has-error");
+}
+else if (passEntered.length > 20) {
+  document.getElementById("passwordError").innerHTML="password cant be more than 20 characters";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
